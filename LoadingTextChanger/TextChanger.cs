4137cc022b6e27fae793c8841c0f1b095c878112
Tutur716 <tutur716@gmail.com>
2018-05-06 00:56:49 +0200

    the sourcecode of the LoadingTextChanger
    under MIT license.

---
+++ LoadingTextChanger/TextChanger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using TMPro;
+
+/// <summary>
+/// This Plugin is under the MIT License and was made by Arthur C.
+/// (@Tutur on ksp forums)
+/// </summary>
+namespace LoadingTextChanger
+{
+    [KSPAddon(KSPAddon.Startup.EveryScene, false)]
+    public class TextChanger : MonoBehaviour
+    {
+        string LoadingText = "Wasting your time..";
+
+        void Awake()
+        {
+            GameObject loadingGO = GameObject.Find("Text");
+            TextMeshPro t = loadingGO.GetComponent<TMPro.TextMeshPro>();
+            t.text = LoadingText;
+        }
+    }
+}
