a301c61979f7e8455c5ff58b426b0bd75abf9e9e
Dorian Stoll <dorian.stoll@tmsp.io>
2018-05-06 01:25:00 +0200

    idk, I am bored

--- LoadingTextChanger/TextChanger.cs
+++ LoadingTextChanger/TextChanger.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using UnityEngine;
 using TMPro;
 
@@ -10,13 +14,25 @@ namespace LoadingTextChanger
     [KSPAddon(KSPAddon.Startup.EveryScene, false)]
     public class TextChanger : MonoBehaviour
     {
-        string LoadingText = "Wasting your time..";
-
         void Awake()
         {
-            GameObject loadingGO = GameObject.Find("Text");
-            TextMeshPro t = loadingGO.GetComponent<TMPro.TextMeshPro>();
-            t.text = LoadingText;
+            List<String> texts = new List<String>();
+            foreach (ConfigNode node in GameDatabase.Instance.GetConfigs("LoadingTextChanger").Select(c => c.config))
+            {
+                foreach (String text in node.GetValues("text"))
+                {
+                    texts.Add(text);
+                }
+            }
+            
+            if (texts.Count > 0)
+            {
+                string LoadingText = texts[new System.Random().Next(0, texts.Count)];
+                        
+                GameObject loadingGO = GameObject.Find("Text");
+                TextMeshPro t = loadingGO.GetComponent<TMPro.TextMeshPro>();
+                t.text = LoadingText;
+            }
         }
     }
 }
